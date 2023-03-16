f48379519db195464a29c5d6e836117469f9a90c
Prafful Gupta <praffulgupta09@gmail.com>
2023-03-16 08:06:53 +0530

    Tokens ☀️

--- app/User/UserServices.js
+++ app/User/UserServices.js
@@ -1,4 +1,85 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const secretKey = "My_SECRET_KEY";
+
+const generateToken = (data, exp) => {
+  if (!exp) exp = Date.now() / 1000 + 24 * 60 * 60;
+
+  const token = jwt.sign(
+    {
+      exp,
+      data,
+    },
+    secretKey
+  );
+  return token;
+};
+
+const decodeToken = (token) => {
+  let data;
+  try {
+    data = jwt.verify(token, secretKey);
+  } catch (_e) {
+    console.log("Error verifying token");
+  }
+
+  return data;
+};
+
+const generateNewAccessToken = async (req, res) => {
+  const { refreshToken } = req.body;
+  if (!refreshToken) {
+    res.status(400).json({
+      status: false,
+      message: `Refresh token required`,
+    });
+    return;
+  }
+
+  const user = await userSchema.findOne({
+    "tokens.refreshToken.token": refreshToken,
+  });
+
+  if (!user) {
+    res.status(422).json({
+      status: false,
+      message: "User not found",
+    });
+    return;
+  }
+
+  const aTokenExp = Date.now() / 1000 + 24 * 60 * 60;
+  const aToken = generateToken(
+    {
+      email: user.email,
+      name: user.name,
+    },
+    aTokenExp
+  );
+
+  user.tokens.accessToken = {
+    token: aToken,
+    expireAt: new Date(aTokenExp * 1000),
+  };
+
+  user
+    .save()
+    .then((user) => {
+      res.status(201).json({
+        status: true,
+        message: "Access token created",
+        data: user,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        status: false,
+        message: "Error creating access token",
+        error: err,
+      });
+    });
+};
 
 const userSchema = require("./UserSchema");
 
@@ -28,11 +109,38 @@ const signupUser = async (req, res) => {
     }
     
     const hashedPassword = await bcrypt.hash(password, 10);
+    
+    const aTokenExp = Date.now() / 1000 + 24 * 60 * 60;
+    const rTokenExp = Date.now() / 1000 + 20 * 24 * 60 * 60;
+    const aToken = generateToken(
+        {
+        email,
+        name,
+        },
+        aTokenExp
+    );
+    const rToken = generateToken(
+        {
+        email,
+        name,
+        },
+        rTokenExp
+    );
 
     const newUser = new userSchema({
         name, 
         email,
         password: hashedPassword,
+        tokens: {
+            accessToken: {
+              token: aToken,
+              expireAt: new Date(aTokenExp * 1000),
+            },
+            refreshToken: {
+              token: rToken,
+              expireAt: new Date(rTokenExp * 1000),
+            },
+        },
     });
 
     newUser.save().then((user) => {
@@ -101,5 +209,7 @@ const loginUser = async (req, res) => {
 };
 
 module.exports = {
-    signupUser, loginUser,
+    signupUser, 
+    loginUser, 
+    generateNewAccessToken,
 }
