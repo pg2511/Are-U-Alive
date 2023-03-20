932c7d1f09b07c9eca4c040aafcef4925081fe21
Prafful Gupta <praffulgupta09@gmail.com>
2023-03-20 16:43:29 +0530

    Auto Login Functionality 🧬

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,76 @@
-import React from 'react';
-import './App.css';
-import Auth from './components/Auth/Auth';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Auth from "./components/Auth/Auth";
 
 function App() {
+  const [pageLoaded, setPageLoaded] = useState(false);
+  const [showAuth, setShowAuth] = useState(false);
+
+  const init = async () => {
+    const rawTokens = localStorage.getItem("tokens");
+    if (!rawTokens) {
+      setShowAuth(true);
+      setPageLoaded(true);
+      return;
+    }
+    const tokens = JSON.parse(rawTokens);
+
+    const accessToken = tokens.accessToken;
+    const aExpiry = new Date(accessToken.expireAt);
+    if (new Date() > aExpiry) {
+      const res = await fetch("http://localhost:5000/user/new-token", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({
+          refreshToken: tokens?.refreshToken?.token,
+        }),
+      }).catch((err) => void err);
+
+      if (!res) {
+        setPageLoaded(true);
+        setShowAuth(true);
+        localStorage.removeItem("tokens");
+        return;
+      }
+
+      const data = await res.json();
+      if (!data || !data.status) {
+        setPageLoaded(true);
+        setShowAuth(true);
+        localStorage.removeItem("tokens");
+        return;
+      }
+
+      const newTokens = data.data?.tokens;
+      localStorage.setItem("tokens", JSON.stringify(newTokens));
+
+      setPageLoaded(true);
+      setShowAuth(false);
+    } else {
+      setPageLoaded(true);
+      setShowAuth(false);
+    }
+  };
+
+  useEffect(() => {
+    init();
+  }, []);
+
   return (
     <div className="app">
-      <Auth/>
+      {pageLoaded ? (
+        showAuth ? (
+          <Auth />
+        ) : (
+          <div className="inner-app"></div>
+        )
+      ) : (
+        <div className="loading">
+          <p>LOADING...</p>
+        </div>
+      )}
     </div>
   );
 }
