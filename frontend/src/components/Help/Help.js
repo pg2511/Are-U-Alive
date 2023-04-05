64d14394fffb29c96d781f26913fbbdbf7c887df
Prafful Gupta <praffulgupta09@gmail.com>
2023-04-06 00:50:41 +0530

    Help section 💁🏻

--- frontend/src/components/Help/Help.js
+++ frontend/src/components/Help/Help.js
@@ -1,17 +1,25 @@
 import React from 'react'
 import styled from 'styled-components'
 import { InnerLayout } from '../../styles/Layouts';
+import help from '../../assets/help.png'
 
 const Help = () => {
     return (
       <HelpStyled>
           <InnerLayout>
+            <h1>Process</h1>
+            <img src={help} className='helpimg'/>
           </InnerLayout>
       </HelpStyled>
     )
   }
   
   const HelpStyled = styled.div`
+      .helpimg {
+        margin: auto 350px;
+        height: 33.5rem;
+        width: 25rem;
+      }
   `
 
 export default Help
